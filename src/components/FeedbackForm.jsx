bfa5100b50a5d238900acbe848c06570b7f028e9
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-05-15 15:45:32 -0300

    section 32 update

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,57 +1,65 @@
-import React from 'react'
-import Card from './shared/Card'
-import {useState} from 'react'
-import Button from './shared/Button'
-import RatingSelect from './RatingSelect'
-function FeedbackForm({handleAdd}) {
-    const [text, setText] = useState('')
-    const [rating,setRating] = useState(10)
-    const [btnDisabled, setBtnDisabled] = useState(true)
-    const [message, setMessage] = useState('')
-    const [selected, setSelected] = useState(1)
-    const handleTextChange = (e) => {
-        if (text === ''){
-            setBtnDisabled(true)
-            setMessage(null)
-        } else if(text != '' && text.trim().length <= 8){
-            setMessage('At least 10 char')
-            setBtnDisabled(true)
-        } else{
-            setMessage(null)
-            setBtnDisabled(false)
-        }
-        setText(e.target.value)
+import React from "react";
+import Card from "./shared/Card";
+import { useState, useContext } from "react";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
+import FeedbackContext from "../context/FeedbackContext";
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const [selected, setSelected] = useState(1);
+  const { addFeedback } = useContext(FeedbackContext);
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text != "" && text.trim().length <= 8) {
+      setMessage("At least 10 char");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (text.trim().length > 10){
-            const newFeedback = {
-                text,
-                rating,
-            }
-            handleAdd(newFeedback)
-            setText('')
-        }
+    setText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      addFeedback(newFeedback);
+      setText("");
     }
-
+  };
 
   return (
     <Card>
-        <form onSubmit={handleSubmit}>
-            <h2>Rate us</h2>
-            <RatingSelect select={(rating) => setRating(rating)} selected={selected} setSelected={setSelected}/>
-            <div className='input-group'>
-                <input
-                onChange={handleTextChange} 
-                type='text' 
-                placeholder='Write a review'
-                value={text} />
-                <Button type="submit" version='secondary' isDisabled={btnDisabled}>Send</Button>
-            </div>
-            {message && <div className='message'>{message}</div>}
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h2>Rate us</h2>
+        <RatingSelect
+          select={(rating) => setRating(rating)}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" version="secondary" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
     </Card>
-  )
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,21 @@
-import {FaTimes} from 'react-icons/fa'
-import Card from "./shared/Card"
-import PropTypes from 'prop-types'
-function FeedbackItem({item, handleDelete}) {
-    
-    return (
-        <Card>
-            <div className="num-display">{item.rating}</div>
-            <button onClick={() => handleDelete(item.id)} className='close'>
-                <FaTimes color='purple'/>
-            </button>
-            <div className="text-display">{item.text}</div>
-        </Card>
-    )
+import { FaTimes } from "react-icons/fa";
+import { useContext } from "react";
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
+function FeedbackItem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
 }
 FeedbackItem.propTypes = {
-    item: PropTypes.object.isRequired,
-}
-export default FeedbackItem
+  item: PropTypes.object.isRequired,
+};
+export default FeedbackItem;
